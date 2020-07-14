6a16e56da57d068e0323537a243142ac836baf92
freemasons <shivasmile11@gmail.com>
2020-07-14 17:48:51 +0300

    Updates

---
+++ precache-manifest.990d7ebefe9551a16ff2a59ac7ae6b86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b63231914f21fe4b304caf3ff71c7466",
+    "url": "/voodooo/index.html"
+  },
+  {
+    "revision": "8b28fe171c3689c962e3",
+    "url": "/voodooo/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "bfc62695ec444bde06ae",
+    "url": "/voodooo/static/css/main.ef146826.chunk.css"
+  },
+  {
+    "revision": "8b28fe171c3689c962e3",
+    "url": "/voodooo/static/js/2.e946d772.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/voodooo/static/js/2.e946d772.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfc62695ec444bde06ae",
+    "url": "/voodooo/static/js/main.3606395b.chunk.js"
+  },
+  {
+    "revision": "60dd8615d6762efa1d46",
+    "url": "/voodooo/static/js/runtime-main.99950245.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/voodooo/precache-manifest.990d7ebefe9551a16ff2a59ac7ae6b86.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/voodooo/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
